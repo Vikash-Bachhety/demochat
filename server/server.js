3ec560b8ed1de91161311e236a6f35af8130b5bc
Vikash Bachhety <144031359+Vikash-Bachhety@users.noreply.github.com>
2024-07-05 22:30:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -84,7 +84,7 @@ io.on("connection", async (socket) => {
   // });
 });
 
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
